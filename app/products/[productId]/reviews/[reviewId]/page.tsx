be290240f5398ea88a726d800bbd94443b8996a4
throwaway <throwaway.zt123@gmail.com>
2026-07-13 15:35:15 -0700

    fix: make dynamic route params asynchronous for Next 16 compatibility

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,20 @@
+// app/products/[productId]/reviews/[reviewId]/page.tsx
+
 type Props = {
-  params: {
+  params: Promise<{
     productId: string;
     reviewId: string;
-  };
+  }>;
 };
 
-export default function ProductReview({ params }: Props) {
+// 1. Add "async" to the function
+export default async function ProductReview({ params }: Props) {
+  // 2. "await" the params promise before using the variables
+  const { productId, reviewId } = await params;
+
   return (
     <h1>
-      Review Details {params.productId} and review = {params.reviewId}
+      Review Details {productId} and review = {reviewId}
     </h1>
   );
 }
